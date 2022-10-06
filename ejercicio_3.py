2cf330ef1906f9c10c94e6a3554deaeda82274e6
MatiasJMB <matias.moreno.b@alumnos.upla.cl>
2022-10-05 22:02:46 -0300

    actualizacion

---
+++ ejercicio_3.py
@@ -0,0 +1,18 @@
+# Ejercicio 3 (18 puntos): Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
+
+def main():
+    print("# Para terminar ingrese 'salir'\n")
+    cadena = str()
+    while (cadena != "salir"):
+        cadena = input("Ingrese una cadena de texto: ")
+        if (cadena == "salir"):
+            print("Programa finalizado.")
+        else:
+            print("⮕ Texto ingresado: " + cadena)
+    
+if __name__ == '__main__':
+    main()
+    
+'''
+¯\_( ͡° ͜ʖ ͡°)_/¯ Matias Moreno Barrios
+'''
